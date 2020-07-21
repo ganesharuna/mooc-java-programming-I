fd1089111894741b993fdd6bd811a742a61fc590
ganesharuna <66627443+ganesharuna@users.noreply.github.com>
2020-07-21 10:34:01 -0500

    Add files via upload

---
+++ part03-Part03_21.PrintNeatly/src/main/java/ArrayPrinter.java
@@ -0,0 +1,26 @@
+
+public class ArrayPrinter {
+
+    public static void main(String[] args) {
+        // You can test your method here
+        int[] array = {5, 1, 3, 4, 2};
+        printNeatly(array);
+    }
+
+    public static void printNeatly(int[] array) {
+        System.out.print(array[0]);
+        for (int i=1;i<array.length;i++){
+            System.out.print(","+array[i]);
+           
+        
+        
+        
+    }
+}
+            
+            
+            
+            
+        
+}
+
